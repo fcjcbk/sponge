871361505dc5957b916648231827270302d22240
Your Name <you@example.com>
2023-03-09 19:08:53 +0800

    完成了部分tcp_receiver但是在ackno上仍存在一些问题

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,4 +1,6 @@
 #include "tcp_receiver.hh"
+#include "wrapping_integers.hh"
+#include <cstdint>
 
 // Dummy implementation of a TCP receiver
 
@@ -12,8 +14,59 @@ using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
     DUMMY_CODE(seg);
+    // 此处需要判断是否为fin，同时为fin要进行下一步操作
+    if (seg.header().syn == true) {
+        if (is_start == false) {
+            is_start = true;
+            isn = seg.header().seqno;
+        }
+    }
+    if (is_start == false) {
+        return;
+    }
+    uint64_t absolute_index = unwrap(seg.header().seqno, isn, _reassembler.get_current_index());
+
+    // bool flag = false; // 在参数传递时判断是否为eof
+    if (seg.header().fin == true) {
+        if (is_end == false) {
+            is_end = true;
+            if (seg.header().syn == true) {
+                end_index = absolute_index + seg.length_in_sequence_space() - 2; // 由于fin和syn同时存在
+            } else {
+                end_index = absolute_index + seg.length_in_sequence_space() - 1; // 由于fin和syn存在
+            }
+        }
+    }
+    if (absolute_index == 0) {
+        write_to_assembler(seg.payload().copy(), absolute_index);
+    } else {            
+        write_to_assembler(seg.payload().copy(), absolute_index - 1);
+    }
+    // if (seg.header().syn == true) {}
+    
+
+
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (is_start == false) {
+        return {};
+    }
+    // 要对标志位作处理
+    uint64_t absolute_index = _reassembler.get_current_index() + 1ul;
+    if (is_end && _reassembler.get_current_index() == end_index) {
+        absolute_index++;
+    }
+    return wrap(absolute_index, isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _reassembler.remaining_capacity(); }
+
+void TCPReceiver::write_to_assembler(const std::string& data, uint64_t index) {
+    if (is_end && index + data.size() < end_index) {
+        _reassembler.push_substring(data, index, is_end);
+    } else {
+        size_t write_size = end_index - index;
+        _reassembler.push_substring(data.substr(0, write_size), index, is_end);
+    }
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -6,6 +6,7 @@
 #include "tcp_segment.hh"
 #include "wrapping_integers.hh"
 
+#include <cstdint>
 #include <optional>
 
 //! \brief The "receiver" part of a TCP implementation.
@@ -20,12 +21,22 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool is_start = false;
+
+    bool is_end = false;
+
+    WrappingInt32 isn;
+
+    uint64_t end_index = 0; // 第一个不能收到的就是比最后一个index多1 stream_index 不包括fin 和 syn
+
+    void write_to_assembler(const std::string& data, uint64_t index);
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
